93c151dd5af6981ca7e6e5c250ce5cff94b4f494
airlinesfarefinder <airlinesfarefinder@gmail.com>
2025-07-11 04:22:50 +0530

    update

--- inc/head.php
+++ inc/head.php
@@ -6,7 +6,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <title>AllInOne</title>
+    <title>TravelnowDeals</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
